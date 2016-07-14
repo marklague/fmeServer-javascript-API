d2ce18cec44b9c49ce150f3b7c4d19a9a3cf12c2
marklague <marklague@users.noreply.github.com>
2016-07-14 09:55:00 -0400

    Update demo.js

--- sample/js-includes/demo.js
+++ sample/js-includes/demo.js
@@ -21,73 +21,73 @@ var src = '../sample/config/fmeCfg.txt';
 objConfig.init(src, lng, addServices);
 
 function addServices(objConfig){
-  //get configuration objects, that are the same for all services
-var user = objConfig.getConfigElement('GuestAccount').data;
-var setting = objConfig.getConfigElement('defaultSettings').data;
-var container = objConfig.getConfigElement('HTMLContainers').data;
-var selectTitle = {};
-selectTitle.en = 'Select Service';
-selectTitle.fr = 'Sélectionnez service';
-//get all services from the configuration file. 
-var services = objConfig.getConfigElement('fmeServices').data;
-
-//build links for each service in the configuration file
-jQuery(document)
-    .on("wb-ready.wb", function () {
-        'use strict';
-        jQuery.each(services, function (key, value) {
-            var templ = linkTmpl;
-            templ = templ.replace('{id}', value.serviceID);
-            templ = templ.replace('{name}', value.serviceTitle[lng]);
-            templ = templ.replace('{href}', value.serviceID);
-            servicesLinks = servicesLinks + templ;
-        });
-
-        
-        if (servicesLinks.length > 0) {
-            //display the links to the service
-            servicesLinks = '<ul class="list-group"><h2>' + selectTitle[lng] + ':</h2>' + servicesLinks + '</ul>';
-            jQuery('#FMEServices')
-                .append(servicesLinks);
+    //get configuration objects, that are the same for all services
+    var user = objConfig.getConfigElement('GuestAccount').data;
+    var setting = objConfig.getConfigElement('defaultSettings').data;
+    var container = objConfig.getConfigElement('HTMLContainers').data;
+    var selectTitle = {};
+    selectTitle.en = 'Select Service';
+    selectTitle.fr = 'Sélectionnez service';
+    //get all services from the configuration file. 
+    var services = objConfig.getConfigElement('fmeServices').data;
+    
+    //build links for each service in the configuration file
+    jQuery(document)
+        .on("wb-ready.wb", function () {
+            'use strict';
+            jQuery.each(services, function (key, value) {
+                var templ = linkTmpl;
+                templ = templ.replace('{id}', value.serviceID);
+                templ = templ.replace('{name}', value.serviceTitle[lng]);
+                templ = templ.replace('{href}', value.serviceID);
+                servicesLinks = servicesLinks + templ;
+            });
+    
             
-            //add on click event to each Service link
-            jQuery("#FMEServices a").click(function (event) {
-                    serviceID = jQuery(this).attr('id');
-                    jQuery("#FMENRCanMessageSection").html('');
-                    jQuery("#FMENRCanFormSection").html('');
-                    event.preventDefault();
-                    //get configuration object for the selected service
-                    service = objConfig.getConfigService(serviceID).data;
-                    //name of the extraction zone field will be used for synchronization with 
-                    geometryField = service.extractionZoneParamName;
-                    //initiate service object
-                    objFME.init(user, service, setting, container, lng);
-                     //get service token
-                    objFME.getToken(service.serviceID, user.username, user.password, proceedToken);
-                });
-        }
-
-         //callback function for getToken function
-        function proceedToken(token) {
-            'use strict';
-            //get service parameters
-            if (token !=="")
-            {
-            objFME.getWebServiceParam(service, token, proceedServiceParam);
-        }
-        }
-
-        //callback function for getWebServiceParam
-        function proceedServiceParam(json) {
-            'use strict';
-            objFME.buildForm(json);
-             jQuery('#FMENRCanFormSection')
-                        .attr('tabindex', -1)
-                        .focus();
-        }   
-
-    });
-}
+            if (servicesLinks.length > 0) {
+                //display the links to the service
+                servicesLinks = '<ul class="list-group"><h2>' + selectTitle[lng] + ':</h2>' + servicesLinks + '</ul>';
+                jQuery('#FMEServices')
+                    .append(servicesLinks);
+                
+                //add on click event to each Service link
+                jQuery("#FMEServices a").click(function (event) {
+                        serviceID = jQuery(this).attr('id');
+                        jQuery("#FMENRCanMessageSection").html('');
+                        jQuery("#FMENRCanFormSection").html('');
+                        event.preventDefault();
+                        //get configuration object for the selected service
+                        service = objConfig.getConfigService(serviceID).data;
+                        //name of the extraction zone field will be used for synchronization with 
+                        geometryField = service.extractionZoneParamName;
+                        //initiate service object
+                        objFME.init(user, service, setting, container, lng);
+                         //get service token
+                        objFME.getToken(service.serviceID, user.username, user.password, proceedToken);
+                    });
+            }
+    
+             //callback function for getToken function
+            function proceedToken(token) {
+                'use strict';
+                //get service parameters
+                if (token !=="")
+                {
+                objFME.getWebServiceParam(service, token, proceedServiceParam);
+            }
+            }
+    
+            //callback function for getWebServiceParam
+            function proceedServiceParam(json) {
+                'use strict';
+                objFME.buildForm(json);
+                 jQuery('#FMENRCanFormSection')
+                            .attr('tabindex', -1)
+                            .focus();
+            }   
+    
+        });
+    }
 
 wb.doc.on("geomap.ready", function () {
     'use strict';
